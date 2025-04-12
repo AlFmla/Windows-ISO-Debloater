55e3f59722cdb33255fe78d0453511e9a0e62d15
Al <afmla@yandex.ru>
2025-04-12 17:39:33 +0300

    Update download.ps1
    url = alfmla

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 # $scriptUrl = "https://raw.githubusercontent.com/itsNileshHere/Windows-ISO-Debloater/main/isoDebloaterScript.ps1"
-$scriptUrl = "https://itsnileshhere.github.io/Windows-ISO-Debloater/isoDebloaterScript.ps1"
-$autounattendXmlUrl = "https://itsnileshhere.github.io/Windows-ISO-Debloater/autounattend.xml"
+$scriptUrl = "https://alfmla.github.io/Windows-ISO-Debloater/isoDebloaterScript.ps1"
+$autounattendXmlUrl = "https://alfmla.github.io/Windows-ISO-Debloater/autounattend.xml"
 
 $scriptDirectory = "$env:G:\scriptdir"
 
